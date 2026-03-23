fdeab6bd84b3730edfff45e83f555992546c98d8
cendy Andreoli <cendyandreoli@gmail.com>
2026-03-22 23:39:53 -0300

    fix: pass localeMap as third arg to createFromSource

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,7 +1,7 @@
 import { source } from "../../../lib/source";
 import { createFromSource } from "fumadocs-core/search/server";
 
-export const { GET } = createFromSource(source, {
+export const { GET } = createFromSource(source, undefined, {
   localeMap: {
     "pt-BR": "portuguese",
   },
